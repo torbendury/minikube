cf0e46aed83b84a985d996b6ab284f11b61a33c2
Torben Dury <torbendury@googlemail.com>
2024-03-12 21:21:47 +0100

    feat(istio): static redirects

--- applications/service-a/main.go
+++ applications/service-a/main.go
@@ -21,37 +21,41 @@ func main() {
 	delayer = time.Tick(seconds * time.Second)
 
 	srv.HandleFunc("/service-a", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello from service A"))
+		w.Write([]byte("Hello from service A\n"))
 	})
 
 	srv.HandleFunc("/random-fail", func(w http.ResponseWriter, r *http.Request) {
 		if oopsie() {
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Failed"))
+			w.Write([]byte("Failed\n"))
 			return
 		}
-		w.Write([]byte("Success"))
+		w.Write([]byte("Success\n"))
 	})
 
 	srv.HandleFunc("/random-delay", func(w http.ResponseWriter, r *http.Request) {
 		if oopsie() {
 			<-delayer
 		}
-		w.Write([]byte("Success"))
+		w.Write([]byte("Success\n"))
 	})
 
 	srv.HandleFunc("/retriable", func(w http.ResponseWriter, r *http.Request) {
 		if oopsie() {
 			w.Header().Set("x-try-again", "true")
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("Failed"))
+			w.Write([]byte("Failed\n"))
 			return
 		}
-		w.Write([]byte("Success"))
+		w.Write([]byte("Success\n"))
 	})
 
 	srv.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
+	})
+
+	srv.HandleFunc("/new-route", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("sorry we didnt tell you about the new route lol\n"))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", srv))
